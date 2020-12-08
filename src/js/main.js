f06c1c7acdadd0c9142a85c639d11c2edae49f5f
yarbest <72338354+yarbest@users.noreply.github.com>
2020-12-08 16:51:30 +0200

    заменил формат картинок

--- src/js/main.js
+++ src/js/main.js
@@ -31,11 +31,25 @@ toggleSidebar.addEventListener("click", () => {
     // body.classList.toggle("lock-scroll");
 });
 
-if (sidebar.clientHeight > "58") {
-    // sidebar.style.bottom = "-118px";
-}
-console.log(sidebar.offsetHeight);
 // /Открытие меню----------------------------------------------------------------------------
+
+//Функция для поддержки webp в css -------------------------------------------------------
+function testWebP(callback) {
+    var webP = new Image();
+    webP.onload = webP.onerror = function () {
+        callback(webP.height == 2);
+    };
+    webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
+testWebP(function (support) {
+    if (support == true) {
+        document.querySelector("body").classList.add("webp");
+    } else {
+        document.querySelector("body").classList.add("no-webp");
+    }
+});
+//Функция для поддержки webp в css -------------------------------------------------------
 
 $(document).ready(function () {
     //preloader------------------------------------------------------------------
